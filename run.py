e74d8d754562360f5146fac0088c4f89a3ea325c
Roman <zakovenkoroma@gmail.com>
2025-04-24 21:14:01 +1000

    Delete admins from .env v2

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
 from config import Config
 
-print(Config.BOT_TOKEN)
+bot = Bot(token=Config.BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
